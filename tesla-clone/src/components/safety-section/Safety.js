b30da8f21d4b1f688254da3189862f4ec8066d16
dap <dalfonzo18@gmail.com>
2020-03-26 18:15:04 -0400

    agregado cambios visuales en safety section

--- tesla-clone/src/components/safety-section/Safety.js
+++ tesla-clone/src/components/safety-section/Safety.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import VerticalAside from '../vertical-aside/VerticalAside';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 import classes from './safety.module.scss';
 
@@ -11,9 +13,18 @@ const Safety = ({ verticalAsideProps, heroImg, carParts }) => {
         <VerticalAside {...verticalAsideProps} />
       </div>
       <div className={classes.rightSide}>
-        <div className={classes.line1}>{carParts.partOne}</div>
-        <div className={classes.line2}>{carParts.partTwo}</div>
-        <div className={classes.line3}>{carParts.partThree}</div>
+        <div className={classes.line1}>
+          <span>1</span>
+          {carParts.partOne} <Icon icon={faAngleDoubleRight} />
+        </div>
+        <div className={classes.line2}>
+          <span>2</span>
+          {carParts.partTwo} <Icon icon={faAngleDoubleRight} />
+        </div>
+        <div className={classes.line3}>
+          <span>3</span>
+          {carParts.partThree} <Icon icon={faAngleDoubleRight} />
+        </div>
         <img src={heroImg} alt="" />
       </div>
     </div>
