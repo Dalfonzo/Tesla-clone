edf3ef6ff43e20fa360dd1b8a78cbc54027d0a2e
dap <dalfonzo18@gmail.com>
2020-03-29 17:51:48 -0400

    performance querys terminados

--- tesla-clone/src/components/specs/car-horizontal-specs/HorizontalSpecs.js
+++ tesla-clone/src/components/specs/car-horizontal-specs/HorizontalSpecs.js
@@ -10,34 +10,31 @@ const HorizontalSpecs = props => {
     changeColor = { filter: 'invert(1)' };
   }
 
-  let border = null;
-  if (!props.button) {
-    border = { borderLeft: '0.1px solid var(--secondary-color)' };
-  }
-
   return (
     <div className={classes.container} style={changeColor}>
-      <div className={classes.left}>
-        <div className={classes.specsTitle}>
-          {props.speedometerOnFirstCell && props.speedometer && (
-            <Speedometer invertColor={changeColor} />
-          )}
-          {props.leftTitle}
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <div className={classes.specsTitle}>
+            {props.speedometerOnFirstCell && props.speedometer && (
+              <Speedometer invertColor={changeColor} />
+            )}
+            {props.leftTitle}
+          </div>
+          <p className={classes.specstSubTitle}>{props.leftText}</p>
         </div>
-        <p className={classes.specstSubTitle}>{props.leftText}</p>
-      </div>
-      <div className={classes.mid}>
-        <div className={classes.specsTitle}>
-          {!props.speedometerOnFirstCell && props.speedometer && (
-            <Speedometer invertColor={changeColor} />
-          )}
-          {props.midTitle}
+        <div className={classes.mid}>
+          <div className={classes.specsTitle}>
+            {!props.speedometerOnFirstCell && props.speedometer && (
+              <Speedometer invertColor={changeColor} />
+            )}
+            {props.midTitle}
+          </div>
+          <p className={classes.specstSubTitle}>{props.midText}</p>
         </div>
-        <p className={classes.specstSubTitle}>{props.midText}</p>
-      </div>
-      <div className={classes.right} style={border}>
-        <p className={classes.specsTitle}>{props.rightTitle}</p>
-        <p className={classes.specstSubTitle}>{props.rightText}</p>
+        <div className={classes.right}>
+          <p className={classes.specsTitle}>{props.rightTitle}</p>
+          <p className={classes.specstSubTitle}>{props.rightText}</p>
+        </div>
       </div>
       {props.button && <OrderNowBtn invertColor={changeColor} />}
     </div>
